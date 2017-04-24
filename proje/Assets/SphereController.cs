9f172e0510d3537219508bf7609833f01214d040
sedaaslan <sdnrsln@gmail.com>
2017-04-25 00:48:19 +0300

    First Comment
    Added Unity Project to Github

---
+++ proje/Assets/SphereController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+
+    public float speed;
+
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //    transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * 4 * Time.deltaTime);
+    //    transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * 4 * Time.deltaTime);
+
+    //}
+
+
+}
